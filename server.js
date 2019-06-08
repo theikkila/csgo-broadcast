7a65f7cf933b95ca95ba9bddc1f5eaa641dc6986
Flowing <bitoreieru@gmail.com>
2019-06-08 17:04:54 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,54 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const swig  = require('swig');
+const swig = require('swig');
 
 const levelup = require('level');
 
-let db = levelup('./csgo', {valueEncoding: 'json'});
+let db = levelup('./csgo', { valueEncoding: 'json' });
 
 db.get('shows', (err, shows) => {
   if (err) {
-    db.put('shows', {shows:[]});
+    db.put('shows', { shows: [] });
   }
 });
 
 function frame_buffer_put(token, fragment_number, tick) {
-  db.get(token+'-framebuffer', (err, framebuffer) => {
+  db.get(token + '-framebuffer', (err, framebuffer) => {
     if (err) {
       framebuffer = [];
     }
-    framebuffer.push({fragment_number: fragment_number, tick: tick});
+    framebuffer.push({ fragment_number: fragment_number, tick: tick });
     if (framebuffer.length >= 5) {
       framebuffer.shift()
     }
     console.log(framebuffer);
-    db.put(token+'-framebuffer', framebuffer);
+    db.put(token + '-framebuffer', framebuffer);
   })
 }
 
 
 app.get('/', function (req, res) {
-  const t = swig.compileFile(__dirname+'/views/plays.html');
+  const t = swig.compileFile(__dirname + '/views/plays.html');
   db.get('shows', (err, shows) => {
     res.send(t(shows))
   });
 });
 
-function set_started (token, start_fragment, auth) {
+function set_started(token, start_fragment, auth) {
   db.get('shows', (err, shows) => {
-      let s = shows.shows;
-      s.push({token: token, timestamp: (new Date().toJSON()), auth: auth})
-      db.put('shows', {shows: s});
+    let s = shows.shows;
+    s.push({ token: token, timestamp: (new Date().toJSON()), auth: auth })
+    db.put('shows', { shows: s });
   });
-  db.put(token+'-started', start_fragment)
+  db.put(token + '-started', start_fragment)
 }
 
-function set_fullpoint (token, cb) {
-  db.get(token+'-framebuffer', (err, framebuffer) => {
+function set_fullpoint(token, cb) {
+  db.get(token + '-framebuffer', (err, framebuffer) => {
     if (err) return cb();
     const frame = framebuffer[0];
-    db.put(token+'-state', {fragment: frame.fragment_number, tick: frame.tick}, cb);
+    db.put(token + '-state', { fragment: frame.fragment_number, tick: frame.tick }, cb);
   })
 }
 
@@ -60,44 +60,46 @@ app.get('/match/:token/:fragment_number/:frametype', function (req, res) {
     console.log("match play", req.params.token, req.params.fragment_number, req.params.frametype);
     res.setHeader('Content-Type', 'application/octet-stream')
     */
-   var p;
-    if (req.params.frametype == 'start') {
-      //console.log("starting", req.params.token, "with fragment_number", req.params.fragment_number);
-      p = fragments_start[req.params.fragment_number]
-    }
-    if (req.params.frametype == 'full') {
-        //console.log("Fragment", req.params.fragment_number, "for tick", req.query.tick);
-        p = fragments_full[req.params.fragment_number]
-    }
-    if (req.params.frametype == 'full') {
-     //console.log("Fragment", req.params.fragment_number, "for tick", req.query.tick);
-     p = fragments_delta[req.params.fragment_number]
-   }
-	res.write(p, 'binary');
-	res.end(null, 'binary');
-    //fs.createReadStream(p)
-    //.pipe(res)
+  var p;
+  if (req.params.frametype == 'start') {
+    //console.log("starting", req.params.token, "with fragment_number", req.params.fragment_number);
+    p = fragments_start[req.params.fragment_number]
+  }
+  if (req.params.frametype == 'full') {
+    //console.log("Fragment", req.params.fragment_number, "for tick", req.query.tick);
+    p = fragments_full[req.params.fragment_number]
+  }
+  if (req.params.frametype == 'full') {
+    //console.log("Fragment", req.params.fragment_number, "for tick", req.query.tick);
+    p = fragments_delta[req.params.fragment_number]
+  }
+  res.write(p, 'binary');
+  res.end(null, 'binary');
+  //fs.createReadStream(p)
+  //.pipe(res)
 })
 
 app.get('/match/:token/sync', function (req, res) {
   console.log("match sync!")
   set_fullpoint(req.params.token, () => {
 
-  db.get(req.params.token+'-state', (err, state) => {
-    if (err) return res.status(404).send("not found");
-    console.log(state)
-    db.get(req.params.token+'-started', (err, start) => {
+    db.get(req.params.token + '-state', (err, state) => {
       if (err) return res.status(404).send("not found");
-      console.log(start)
-      const r = {tick: parseInt(state.tick),
-                rtdelay: 1,
-                rcvage: 1,
-                fragment: parseInt(state.fragment),
-                signup_fragment: parseInt(start),
-                tps: 128,
-                protocol: 4};
-      console.log(r)
-      res.send(r);
+      console.log(state)
+      db.get(req.params.token + '-started', (err, start) => {
+        if (err) return res.status(404).send("not found");
+        console.log(start)
+        const r = {
+          tick: parseInt(state.tick),
+          rtdelay: 1,
+          rcvage: 1,
+          fragment: parseInt(state.fragment),
+          signup_fragment: parseInt(start),
+          tps: 128,
+          protocol: 4
+        };
+        console.log(r)
+        res.send(r);
       })
     });
   })
@@ -106,41 +108,35 @@ app.get('/match/:token/sync', function (req, res) {
 
 //  playcast "http://586f7685.ngrok.io/match/s85568392920768736t1477086968"
 app.post('/reset/:token/', (req, res) => {
-  db.del(req.params.token+'-started')
+  db.del(req.params.token + '-started')
   res.send("ACK");
 })
 
 var fragments_start = {};
 var fragments_full = {};
 var fragments_delta = {};
+var started = false;
 app.post('/:token/:fragment_number/:frametype', function (req, res) {
-   db.get(req.params.token+'-started', function (err, value) {
-     if (err && req.params.frametype != 'start') {
-       return res.status(205).send("reset");
-     }
-	 //var fragname = 'datas/'+req.params.token+'_'+req.params.fragment_number+'_'+req.params.frametype;
-	 //fragments[fragname] = req.body;
-     /*const p = fs.createWriteStream('datas/'+req.params.token+'_'+req.params.fragment_number+'_'+req.params.frametype);
-     req.pipe(p)
-     p.on('finish', function(){
-    */
-       if (req.params.frametype == 'start') {
-         console.log("starting", req.params.token, "with fragment_number", req.params.fragment_number);
-         fragments_start[req.params.fragment_number] = req.body
-       }
-       if (req.params.frametype == 'full') {
-           console.log("Fragment", req.params.fragment_number, "for tick", req.query.tick);
-           fragments_full[req.params.fragment_number] = req.body
-       }
-       if (req.params.frametype == 'full') {
-        console.log("Fragment", req.params.fragment_number, "for tick", req.query.tick);
-        fragments_delta[req.params.fragment_number] = req.body
-      }
-       
-     });
-     
-     res.status(200).send("OK");
-   });
+  if (!started) {
+    res.status(205).send("Reset");
+  }
+  if (req.params.frametype == 'start') {
+    console.log("starting", req.params.token, "with fragment_number", req.params.fragment_number);
+    fragments_start[req.params.fragment_number] = req.body
+    started = true
+  }
+  if (req.params.frametype == 'full') {
+    console.log("Fragment", req.params.fragment_number, "for tick", req.query.tick);
+    fragments_full[req.params.fragment_number] = req.body
+  }
+  if (req.params.frametype == 'full') {
+    console.log("Fragment", req.params.fragment_number, "for tick", req.query.tick);
+    fragments_delta[req.params.fragment_number] = req.body
+  }
+  res.status(200).send("OK");
+});
+
+
 
 app.listen(3000, function () {
   console.log('CSGO broadcast server listening on port 3000!');
