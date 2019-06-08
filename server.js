f1b69c8de7b7b4abfa2f5b15d8989cd9bd5b4680
Flowing <bitoreieru@gmail.com>
2019-06-08 18:02:21 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 const swig = require('swig');
+const bodyParser = require('body-parser');
 
 /*
 app.get('/', function (req, res) {
@@ -10,6 +13,7 @@ app.get('/', function (req, res) {
   });
 });
 */
+app.use(bodyParser.raw({ type:'*/*',limit:'50mb',extended: true }));
 
 app.get('/match/:token/:fragment_number/:frametype', function (req, res) {
   /*const p = 'datas/'+req.params.token+'_'+req.params.fragment_number+'_'+req.params.frametype;
@@ -20,17 +24,26 @@ app.get('/match/:token/:fragment_number/:frametype', function (req, res) {
     */
   console.log('Fragment request for',req.params.fragment_number)
   res.setHeader('Content-Type', 'application/octet-stream')
-  var p = Buffer.alloc(16, 0);
+  var p = Buffer.alloc(16, 0, 'binary');
   if (req.params.frametype == 'start') {
     //console.log("starting", req.params.token, "with fragment_number", req.params.fragment_number);
+    //p = fragments_start[req.params.fragment_number]
+    //console.log(fragments_start[req.params.fragment_number])
+    //res.write(fragments_start[req.params.fragment_number],'binary')
     p = fragments_start[req.params.fragment_number]
   }
   if (req.params.frametype == 'delta') {
     //console.log("Fragment", req.params.fragment_number, "for tick", req.query.tick);
+    //p = fragments_delta[req.params.fragment_number]
+    //console.log(fragments_delta[req.params.fragment_number])
+    //res.write(fragments_delta[req.params.fragment_number],'binary')
     p = fragments_delta[req.params.fragment_number]
   }
   if (req.params.frametype == 'full') {
     //console.log("Fragment", req.params.fragment_number, "for tick", req.query.tick);
+    //p = fragments_full[req.params.fragment_number]
+    //console.log(fragments_full[req.params.fragment_number])
+    //res.write(fragments_full[req.params.fragment_number],'binary')
     p = fragments_full[req.params.fragment_number]
   }
   console.log(p)
@@ -70,30 +83,34 @@ var fragments_delta = {};
 var syncdata = {};
 var started = false;
 app.post('/:token/:fragment_number/:frametype', function (req, res) {
+  console.log(req.body)
   console.log("Fragment ", req.params.frametype, req.params.fragment_number, "for tick", req.query.tick);
   if (req.params.frametype == "start") {
     syncdata.start = req.params.fragment_number
     fragments_start[req.params.fragment_number] = req.body
     started = true;
   }
-  else if (!started) {
-    res.status(205).send("Reset");
-    console.log('reset at type :', req.params.frametype)
-    return;
+  else {
+    if (!started) {
+      res.status(205).send("Reset");
+      console.log('reset at type :', req.params.frametype)
+    }
+    else {
+      if(req.params.fragment_number){
+        syncdata["fragment"] = req.params.fragment_number
+      }
+      if(req.query.tick){
+        syncdata["tick"] = req.query.tick
+      }
+      if (req.params.frametype == 'full') {
+        fragments_full[req.params.fragment_number] = req.body
+      }
+      if (req.params.frametype == 'delta') {
+        fragments_delta[req.params.fragment_number] = req.body
+      }
+      res.status(200).send("OK");
+    }
   }
-  if(req.params.fragment_number){
-    syncdata["fragment"] = req.params.fragment_number
-  }
-  if(req.query.tick){
-    syncdata["tick"] = req.query.tick
-  }
-  if (req.params.frametype == 'full') {
-    fragments_full[req.params.fragment_number] = req.body
-  }
-  if (req.params.frametype == 'delta') {
-    fragments_delta[req.params.fragment_number] = req.body
-  }
-  res.status(200).send("OK");
 })
 
 
